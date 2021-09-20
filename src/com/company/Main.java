e2ec9f3b2641b363d817f4c21d6035f81c5b60b8
SebastianHalfdanLauridsen <s215769@dtu.dk>
2021-09-20 16:34:00 +0200

    Yaboi got acces ns

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    	// write your code here
         //jeg kan ikke lide ost og joe har adgang
+        //yaboi slange har adgang (no shit jeg lavede repoet)
     }
 }
